01081099f99a46cc4cf640227caa2b7dd7e35649
Cherng-Ying Ing <cying.ing@gmail.com>
2014-04-05 11:46:19 +0800

    Change VoltageTracker member getVoltage to getValue

--- VoltageTracker.h
+++ VoltageTracker.h
@@ -20,19 +20,19 @@ public:
   {
     pinMode(pin_, INPUT);
     word v = analogRead(pin_);
-    v_ = v_ - getVoltage() + v;
+    v_ = v_ - getValue() + v;
   }
-  word getVoltage(byte extraBits = 0) const
+  word getValue(byte extraBits = 0) const
   {
     return v_ >> (MEASURE_EXTRA_BITS - extraBits);
   }
   void sprint(char* buf) const
   {
   #if PRECISE_VOLTAGE
-    word v = getVoltage(PRESENT_EXTRA_BITS);
+    word v = getValue(PRESENT_EXTRA_BITS);
     word v1000 = (word)((long)v * 5 * 1000 / ((1 << (10 + PRESENT_EXTRA_BITS)) - 1));
   #else
-    word v = getVoltage();
+    word v = getValue();
     word v1000 = v * 5;
   #endif
     sprintf(buf, "%uv%03u", v1000 / 1000, v1000 % 1000);
